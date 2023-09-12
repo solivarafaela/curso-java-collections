235c5c002dd48eb9aac86c6253cb950c39289b41
solivarafaela <rafaelasoliva@outlook.com>
2023-09-12 13:35:21 -0300

    Add classe ListaTarefas

---
+++ java-collections/src/tarefas/ListaTarefas.java
@@ -0,0 +1,40 @@
+package tarefas;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class ListaTarefas {
+
+    private List<Tarefa> listaTarefas;
+
+    public ListaTarefas() {
+        this.listaTarefas = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(String descricao) {
+        listaTarefas.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao) {
+        List<Tarefa> tarefasARemover = new ArrayList<>(); //Lista de tarefas a excluir
+
+        if(!listaTarefas.isEmpty()) {
+            for(Tarefa t : listaTarefas) { //Percorre a lista procurando a tarefa
+                if(t.getDescricao().equalsIgnoreCase(descricao)) { //Comparando as tarefas
+                    tarefasARemover.add(t); //Adicionando a tarefa encontrada a lista de remoção
+                }
+            }
+        }
+
+        listaTarefas.removeAll(tarefasARemover);
+
+    }
+
+    public void obterTarefas () {
+        System.out.println(listaTarefas);
+    }
+
+    public int obterNumeroTarefas () {
+        return listaTarefas.size();
+    }
+}
