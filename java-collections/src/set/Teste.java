f7d63a9fdf62732eef5782c017e8fe5ca92befa9
solivarafaela <rafaelasoliva@outlook.com>
2023-09-12 18:29:48 -0300

    Add: classe Teste

---
+++ java-collections/src/set/Teste.java
@@ -0,0 +1,22 @@
+package set;
+
+public class Teste {
+    public static void main(String[] args) {
+
+        ListaConvidados listaConvidados = new ListaConvidados();
+
+        System.out.println("A lista possui " + listaConvidados.obterNumeroConvidados() + " convidado(s).");
+
+        listaConvidados.adicionarConvidado("Rafaela", 194346);
+        listaConvidados.adicionarConvidado("Maju", 194308);
+        listaConvidados.adicionarConvidado("Sol", 194351);
+
+        System.out.println("A lista possui " + listaConvidados.obterNumeroConvidados() + " convidado(s).");
+
+        listaConvidados.removerConvidadoCodigoConvite(194346);
+
+        System.out.println("A lista possui " + listaConvidados.obterNumeroConvidados() + " convidado(s).");
+
+        listaConvidados.obterLista();
+    }
+}
