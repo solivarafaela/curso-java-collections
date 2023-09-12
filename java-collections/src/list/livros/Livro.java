1c013239ee85b1237bbfa28ac3c0fdae634e6832
solivarafaela <rafaelasoliva@outlook.com>
2023-09-12 16:14:44 -0300

    Add: classe Livro

---
+++ java-collections/src/list/livros/Livro.java
@@ -0,0 +1,46 @@
+package list.livros;
+
+public class Livro {
+    private String nome;
+    private String autor;
+    private int anoPublicacao;
+
+    public Livro(String nome, String autor, int anoPublicacao) {
+        this.nome = nome;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setAnoPublicacao(int anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", autor='" + autor + '\'' +
+                ", anoPublicacao=" + anoPublicacao +
+                '}';
+    }
+}
