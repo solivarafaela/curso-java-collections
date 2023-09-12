c471a7034a58ec06380f466790392e881c5a2c76
solivarafaela <rafaelasoliva@outlook.com>
2023-09-12 13:53:57 -0300

    Add classe Item

---
+++ java-collections/src/compras/Item.java
@@ -0,0 +1,44 @@
+package compras;
+
+public class Item {
+    private String nome;
+    private float preco;
+    private int quantidade;
+
+    public Item(String nome, float preco, int quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return  "Nome: '" + nome +
+                ", Preco: " + preco +
+                ", Quantidade: " + quantidade;
+    }
+}
