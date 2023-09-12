99c5158315ce6c764c658f2ab7a7c9d1c5b292df
solivarafaela <rafaelasoliva@outlook.com>
2023-09-12 18:43:10 -0300

    Add: equals e hashCode na classe Convidado

--- java-collections/src/set/Convidado.java
+++ java-collections/src/set/Convidado.java
@@ -1,5 +1,7 @@
 package set;
 
+import java.util.Objects;
+
 public class Convidado {
 
     private String nome;
@@ -16,6 +18,18 @@ public class Convidado {
 
     public int getCodigoConvite() {
         return codigoConvite;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Convidado convidado)) return false;
+        return getCodigoConvite() == convidado.getCodigoConvite();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCodigoConvite());
     }
 
     @Override
