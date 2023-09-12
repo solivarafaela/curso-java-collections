7cf444fe9de75764ae8c1fd5b64052b1f4ba205e
solivarafaela <rafaelasoliva@outlook.com>
2023-09-12 18:12:50 -0300

    Add: classe Convidado

---
+++ java-collections/src/set/Convidado.java
@@ -0,0 +1,26 @@
+package set;
+
+public class Convidado {
+
+    private String nome;
+    private int codigoConvite;
+
+    public Convidado(String nome, int codigoConvite) {
+        this.nome = nome;
+        this.codigoConvite = codigoConvite;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getCodigoConvite() {
+        return codigoConvite;
+    }
+
+    @Override
+    public String toString() {
+        return nome +
+                "\nCódigo do convite: " + codigoConvite + "\n" + "\n";
+    }
+}
