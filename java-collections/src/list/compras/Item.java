3944413b651c1b2719c60f18812c02dc7f72a0ba
solivarafaela <rafaelasoliva@outlook.com>
2023-09-12 17:10:30 -0300

    Refactor: classe Item

--- java-collections/src/list/compras/Item.java
+++ java-collections/src/list/compras/Item.java
@@ -15,24 +15,12 @@ public class Item {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
     public float getPreco() {
         return preco;
     }
 
-    public void setPreco(float preco) {
-        this.preco = preco;
-    }
-
     public int getQuantidade() {
         return quantidade;
-    }
-
-    public void setQuantidade(int quantidade) {
-        this.quantidade = quantidade;
     }
 
     @Override
