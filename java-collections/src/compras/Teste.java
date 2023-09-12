88f4e5dc938a685090c51b91df58df022375f618
solivarafaela <rafaelasoliva@outlook.com>
2023-09-12 14:22:06 -0300

    Add classe para testes

---
+++ java-collections/src/compras/Teste.java
@@ -0,0 +1,14 @@
+package compras;
+
+public class Teste {
+    public static void main(String[] args) {
+
+        CarrinhoDeCompras carrinhoDeCompras = new CarrinhoDeCompras();
+
+        carrinhoDeCompras.adicionarItem("Chocolate", 2.99F, 2);
+        carrinhoDeCompras.adicionarItem("Leite", 4.99F, 2);
+
+        carrinhoDeCompras.exibirCarrinho();
+        carrinhoDeCompras.exibirResumo();
+    }
+}
